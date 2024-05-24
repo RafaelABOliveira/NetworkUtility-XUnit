86a0ef52249580445b73a2fa0f0a233929f941dd
rafae <rafae@RAFAEL>
2024-05-24 13:07:24 -0300

    Adicionar arquivos de projeto.

---
+++ NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Tests.PingTest
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange - variables, classes, mocks
+
+            //Act
+            var pingService = new NetworkService();
+
+            //Assert
+            Assert.
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            //searchDNS[]
+            //BuildPacket()
+            return "Success: Ping Sent !";
+        }
+    }
+}
